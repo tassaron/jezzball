6742768189e2b408f164a8a9f8c00f157d8354a1
tassaron <rutachigal@gmail.com>
2022-03-04 12:16:24 -0500

    new font

--- ui.js
+++ ui.js
@@ -29,6 +29,8 @@ let prevScore = -1;
 let prevPercent = -1;
 let gameOverDrawn = false;
 let livesColour = "#000";
+const fontStyleSm = "1rem var(--arcade-font)";
+const fontStyleLg = "2.75rem var(--arcade-font)";
 
 export function addUIEventListeners() {
     /* Connect onclick events to HTML "buttons" */
@@ -183,18 +185,18 @@ export function clearUI() {
 }
 
 function drawScore() {
-    uictx.font = "16pt Sans";
+    uictx.font = fontStyleSm;
     uictx.fillStyle = "#000";
     uictx.fillText(`${percent}% Cleared`, grid_size, 20);
     uictx.fillText(
         `Score: ${score}`,
-        grid_size * (px2grid(uicanvas.width) / 2) - 48,
-        20
+        grid_size * (px2grid(uicanvas.width) / 2) - 30,
+        uicanvas.height - 10
     );
 }
 
 function drawLives() {
-    uictx.font = "16pt Sans";
+    uictx.font = fontStyleSm;
     if (timer.diedRecently > 0) {
         if (timer.diedRecently % 15 == 0 && livesColour == "#000") {
             livesColour = "#ff0000";
@@ -212,37 +214,40 @@ function drawLives() {
     } else {
         var livesText = lives.toString();
     }
-    uictx.fillText(`Lives: ${livesText}`, uicanvas.width - 96, 20);
+    uictx.fillText(`Lives: ${livesText}`, uicanvas.width - 72, 20);
 }
 
 function drawGameOver() {
-    uictx.font = "36pt Sans";
+    uictx.font = fontStyleLg;
     uictx.fillStyle = "#33aaff";
     uictx.fillText(
         "Game Over",
-        uicanvas.width / 2 - 124,
+        uicanvas.width / 2 - 120,
         uicanvas.height / 2 - 32
     );
-    uictx.font = "16pt Sans";
+    uictx.font = fontStyleSm;
     uictx.fillText(
         "tap or click to restart",
-        uicanvas.width / 2 - 92,
+        uicanvas.width / 2 - 72,
         uicanvas.height / 2 + 22
     );
-    drawMuffin();
 }
 
 export function drawPauseScreen() {
-    uictx.font = "36pt Sans";
+    uictx.font = fontStyleLg;
     uictx.fillStyle = "#333";
     uictx.fillText("Paused", uicanvas.width / 2 - 90, uicanvas.height / 2);
 }
 
 function drawCountdown() {
     let num = Math.floor(timer.ballPause / 60) + 1;
-    uictx.font = "36pt Sans";
+    uictx.font = fontStyleLg;
     uictx.fillStyle = purple;
-    uictx.fillText(num, uicanvas.width / 2 - 4, uicanvas.height / 2 + 8);
+    if (num % 2 == 0) {
+        uictx.fillStyle = "black";
+    }
+    uictx.fillText("JEZZBALL", uicanvas.width / 2 - 100, uicanvas.height / 2 - 64);
+    uictx.fillText(num, uicanvas.width / 2 - 8, uicanvas.height / 2 + 4);
 }
 
 function drawMuffin() {
